d048caf054a14597844eae19b46c6b3f80ee4e56
abhinavreddy05 <abhinavreddy05@kgpian.iitkgp.ac.in>
2024-03-20 07:36:54 +0530

    test

--- portal/portal/urls.py
+++ portal/portal/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from faculty import views
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +10,4 @@ urlpatterns = [
     path('optimizing-your-resume/', views.blog1, name='optimizing-your-resume'),
     path('search/', views.search, name='search'),
     path('professor/<slug:professor_name>/', views.professor, name='professor'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
